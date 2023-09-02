b24133f6d4f6dfa325fad8463034aebe482957bc
Matheus <mattcontassot@gmail.com>
2023-09-02 15:36:00 -0300

    ADD: Criação da primeira migration

---
+++ back/src/database/migrations/1693679741971-permissionamentos.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class Permissionamentos1693679741971 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
