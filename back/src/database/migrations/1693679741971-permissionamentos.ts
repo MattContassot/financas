467cafabbbec5dd82b2c0d0f246d702c9c58cd29
Matheus <mattcontassot@gmail.com>
2023-09-02 19:10:18 -0300

    FIX: Linter agora usa extensão js

--- back/src/database/migrations/1693679741971-permissionamentos.ts
+++ back/src/database/migrations/1693679741971-permissionamentos.ts
@@ -1,4 +1,4 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Permissionamentos1693679741971 implements MigrationInterface {
 
